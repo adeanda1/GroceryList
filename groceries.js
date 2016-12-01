6c8532396ae151ec4f8b50d32a0076118c4c97e1
VIBRANIUM\Ariana <adeanda1@mail.csuchico.edu>
2016-11-30 17:26:35 -0800

    Made a for loop.

--- groceries.js
+++ groceries.js
@@ -2,9 +2,22 @@
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 
 
-
+ window.onload = loadCookieList;
 
 var myList = []
+
+function loadCookieList()
+{
+  var food;
+  var str = getCookie("food");
+  var arrayCookie = str.split(",");
+
+  for (food = 0; arrayCookie.length > food; food++ )
+  {
+    displayItem(arrayCookie[food]);
+  }
+
+}
 
 function saveList()
 {
@@ -20,40 +33,42 @@ function clearList()
    list.innerHTML = '';
 }
 
-function addItem()
+function displayItem(foodpicked)
 {
 
+  var index = myList.indexOf(foodpicked);
 
+  if (index == -1) //not in the list, just add, to avoid doubles
+  {
+   myList.push(foodpicked);
+
+   var list = document.getElementById("listDisplay");
+   var item = document.createElement("li");
+   var itemName = document.createTextNode(foodpicked);
+   item.appendChild(itemName);
+   document.getElementById("newItem").value = "";
+   var btnClose = document.createElement("button");
+   btnClose.classList.add("btn");
+   btnClose.classList.add("btn-danger");
+   btnClose.classList.add("btn-xs");
+   btnClose.addEventListener("click", removeParentListItem);
+
+   var iconClose = document.createElement("span");
+   iconClose.classList.add("glyphicon");
+   iconClose.classList.add("glyphicon-remove");
+   btnClose.appendChild(iconClose);
+   item.appendChild(btnClose);
+   list.appendChild(item);
+}
+}
+
+
+function addItem()
+{
   var input = document.getElementById("newItem").value ;
 
-  var index = myList.indexOf(input);
   //myList.push(input);
-   if (index == -1) //not in the list, just add, to avoid doubles
-   {
-    myList.push(input);
-
-    var list = document.getElementById("listDisplay");
-    var item = document.createElement("li");
-    var itemName = document.createTextNode(input);
-    item.appendChild(itemName);
-    document.getElementById("newItem").value = "";
-    var btnClose = document.createElement("button");
-    btnClose.classList.add("btn");
-    btnClose.classList.add("btn-danger");
-    btnClose.classList.add("btn-xs");
-    btnClose.addEventListener("click", removeParentListItem);
-
-    var iconClose = document.createElement("span");
-    iconClose.classList.add("glyphicon");
-    iconClose.classList.add("glyphicon-remove");
-    btnClose.appendChild(iconClose);
-    item.appendChild(btnClose);
-    list.appendChild(item);
-   }
-
-
-
-
+displayItem(input);
 }
 
 function removeParentListItem()
@@ -65,22 +80,27 @@ function removeParentListItem()
   var itemIndex = myList.splice(itemIndex, 1);
 }
 
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays)
+{
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
+function getCookie(cname)
+{
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
+    for(var i = 0; i < ca.length; i++)
+    {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) == ' ')
+        {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) == 0)
+        {
             return c.substring(name.length, c.length);
         }
     }
